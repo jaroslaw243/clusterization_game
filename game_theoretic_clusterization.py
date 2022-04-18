a0520fdc687d764b453f358288769d43d2bb129c
jaroslaw243 <boss.mielczarek@gmail.com>
2022-04-18 23:18:05 +0200

    implemented segmentation algorithm

--- game_theoretic_clusterization.py
+++ game_theoretic_clusterization.py
@@ -4,12 +4,17 @@ import cv2
 
 
 class GameTheoreticClusterization:
-    def __init__(self, image_path, rep_dyn_t_max, sigma=1, load_image_at_start=True):
+    def __init__(self, image_path, rep_dyn_t_max, sigma=1, max_iter=100, load_image_at_start=True):
         self.image_path = image_path
         self.image = None
         self.sigma = np.float64(sigma)
         self.sim_matrix = None
         self.rep_dyn_t_max = rep_dyn_t_max
+        self.indices_vec = None
+        self.first_prob_vec = None
+        self.prob_in_time = None
+        self.max_iter = max_iter
+        self.final_seg = None
 
         if load_image_at_start:
             self.load_image()
@@ -34,4 +39,59 @@ class GameTheoreticClusterization:
         self.sim_matrix = sim_matrix.transpose()
 
     def discrete_replicator_dynamics(self):
-        pass
+        prob_in_time = lil_array((self.rep_dyn_t_max, self.first_prob_vec.shape[1]), dtype=np.float64)
+        prob_in_time[0, :] = lil_array(self.first_prob_vec)
+
+        for i in range(self.rep_dyn_t_max - 1):
+            a = self.sim_matrix @ prob_in_time[[i], :].transpose()
+            b = prob_in_time[[i], :] @ a
+            temp_div = a / b.toarray()
+            prob_in_time[[i + 1], :] = prob_in_time[[i], :] * temp_div.transpose()
+
+        self.prob_in_time = prob_in_time
+
+    def clusterization(self):
+        q = self.image.shape[0] * self.image.shape[1]
+        image_vec = np.reshape(np.transpose(self.image), (1, q))
+        indices_vec = np.arange(stop=q, dtype=int)
+
+        self.generate_similarity_matrix()
+
+        seg = np.zeros((1, q), dtype=np.uint8)
+        iter_n = 1
+        curr_label = 1
+        all_pixels_to_label = q
+        labeled_pixels = 0
+        all_pixels_labeled = False
+        while not all_pixels_labeled and iter_n <= self.max_iter:
+            q = image_vec.shape[0] * image_vec.shape[1]
+            a_init = np.random.uniform(size=(1, q))
+            a_init = a_init/np.sum(a_init)
+            self.first_prob_vec = a_init
+
+            self.discrete_replicator_dynamics()
+
+            prob_increase_ind = self.prob_in_time[[1], :] < self.prob_in_time[[-1], :]
+            prob_increase_ind = prob_increase_ind.toarray()
+            prob_increase_ind_for_rm = np.logical_not(prob_increase_ind)
+            prob_increase_ind_for_rm.shape = q
+
+            image_vec[prob_increase_ind] = np.nan
+            prob_increase_ind.shape = q
+            seg[0, indices_vec[prob_increase_ind]] = curr_label
+
+            indices_vec = indices_vec[prob_increase_ind_for_rm]
+            image_vec = image_vec[0, prob_increase_ind_for_rm]
+            image_vec = np.reshape(image_vec, (1, -1))
+
+            self.sim_matrix = self.sim_matrix[:, prob_increase_ind_for_rm]
+            self.sim_matrix = self.sim_matrix[prob_increase_ind_for_rm, :]
+
+            labeled_pixels += np.sum(np.isnan(image_vec))
+            if labeled_pixels == all_pixels_to_label:
+                all_pixels_labeled = True
+
+            iter_n += 1
+            curr_label += 1
+
+        self.final_seg = np.reshape(seg, self.image.shape)
--- main.py
+++ main.py
@@ -1,8 +1,22 @@
 import cv2
 import numpy as np
+from matplotlib import pyplot as plt
+import matplotlib
 
 from game_theoretic_clusterization import GameTheoreticClusterization
 
 
-clust = GameTheoreticClusterization(image_path='./test_complex3.png', rep_dyn_t_max=50)
-clust.generate_similarity_matrix()
+clust = GameTheoreticClusterization(image_path='./test_complex3.png', rep_dyn_t_max=3)
+clust.clusterization()
+
+matplotlib.use('TkAgg')
+
+fig, ax = plt.subplots(1, 2)
+
+plt.setp(ax, xticks=[], yticks=[])
+ax[0].imshow(clust.image, cmap='gray')
+ax[0].set_title(f'Original (height: {clust.image.shape[0]}px, width: {clust.image.shape[1]}px)')
+ax[1].imshow(clust.final_seg, cmap='gray')
+ax[1].set_title('Segmentation')
+
+plt.show()
