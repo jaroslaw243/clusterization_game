e22ee95ab102248fdc2664c96f2067951d5a6d53
jaroslaw243 <boss.mielczarek@gmail.com>
2022-04-26 01:47:21 +0200

    yaml generation adjusted

--- make_yaml.py
+++ make_yaml.py
@@ -1,8 +1,9 @@
 import yaml
 from game_theoretic_clusterization import GameTheoreticClusterization
 
-clust = GameTheoreticClusterization(image_path='./test_objects.png', rep_dyn_t_max=20, sigma=12,
-                                    use_measure_memory_usage=True, load_image_at_start=False)
+clust = GameTheoreticClusterization(image_path='./test_objects.png', rep_dyn_t_max=20, sigma=12, sigma_dist=2000,
+                                    cluster_size_thresh_perc=0.005, use_measure_memory_usage=True, max_iter=100,
+                                    load_image_at_start=False)
 
 with open(r'.\clust.yaml', 'w') as file:
     documents = yaml.dump(clust, file)
