c7421c39ca2eb5c41a54b50390f590223ed97622
jaroslaw243 <boss.mielczarek@gmail.com>
2022-05-24 00:56:50 +0200

    script version for PH2

--- utils.py
+++ utils.py
@@ -28,3 +28,29 @@ def hair_removal(image):
     ret, thresh2 = cv2.threshold(blackhat, 10, 255, cv2.THRESH_BINARY)
 
     return cv2.inpaint(org_image, thresh2, 1, cv2.INPAINT_TELEA)
+
+
+def hair_removal_and_fill_image_seg_boundaries(image, seg):
+    org_image = copy.copy(image)
+    org_seg = copy.copy(seg)
+    inv_seg = cv2.bitwise_not(org_seg)
+
+    filled_cor1 = cv2.floodFill(inv_seg, None, (0, 0), 1)
+    filled_cor2 = cv2.floodFill(inv_seg, None, (inv_seg.shape[1] - 1, 0), 1)
+    filled_cor3 = cv2.floodFill(inv_seg, None, (0, inv_seg.shape[0] - 1), 1)
+    filled_cor4 = cv2.floodFill(inv_seg, None, (inv_seg.shape[1] - 1, inv_seg.shape[0] - 1), 1)
+
+    comb1 = filled_cor1[1] & filled_cor2[1]
+    comb2 = comb1 & filled_cor3[1]
+    comb3 = comb2 & filled_cor4[1]
+
+    kernel = cv2.getStructuringElement(1, (17, 17))
+
+    blackhat = cv2.morphologyEx(image, cv2.MORPH_BLACKHAT, kernel)
+
+    ret, thresh2 = cv2.threshold(blackhat, 10, 255, cv2.THRESH_BINARY)
+
+    combined_mask = thresh2
+    combined_mask[comb3 == 1] = 255
+
+    return cv2.inpaint(org_image, combined_mask, 1, cv2.INPAINT_TELEA)
